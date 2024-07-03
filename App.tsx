b05c36a829ddb0c59aaa98fd656b4f96f34267e2
Júlia Lima <juliafclima@hotmail.com>
2024-07-03 13:43:06 -0300

    Fontes personalizadas + Componente de Loading

--- App.tsx
+++ App.tsx
@@ -1,5 +1,33 @@
+import {
+  Roboto_400Regular,
+  Roboto_700Bold,
+  useFonts,
+} from "@expo-google-fonts/roboto";
+
+import { Loading } from "./src/components/Loading";
 import { SignIn } from "./src/screens/SignIn";
+import { StatusBar } from "react-native";
+import { ThemeProvider } from "styled-components/native";
+import theme from "./src/theme";
 
 export default function App() {
-  return <SignIn />;
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return <Loading />;
+  }
+
+  return (
+    <ThemeProvider theme={theme}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      <SignIn />
+    </ThemeProvider>
+  );
 }
